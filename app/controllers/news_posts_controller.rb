8972a6eab83077f2f8b74ae24014154a46690c8e
Casey Li <casey.li@gmail.com>
2015-02-23 18:52:34 -0500

    Fleshed out NewsPost resource.

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -4,4 +4,29 @@ class NewsPostsController < ApplicationController
     @news_posts = NewsPost.all
   end
 
+  def show
+    @news_post = NewsPost.find(params[:id])
+  end
+
+  def new
+    @news_post = NewsPost.new
+  end
+
+  def create
+    @news_post = NewsPost.new(news_post_params)
+
+    if @news_post.save
+      # do something - go back to the index
+      redirect_to "/news_posts"
+    else
+      # do something else - present the form again
+    end
+  end
+
+  private
+
+    def news_post_params
+      params.require(:news_post).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,16 @@ Rails.application.routes.draw do
   # NEWS_POST RESOURCE
 
   # new
+  get "/news_posts/new" => "news_posts#new"
 
   # create
+  post "/news_posts" => "news_posts#create"
 
   # index
   get "/news_posts" => "news_posts#index"
 
   # show
+  get "/news_posts/:id" => "news_posts#show"
 
   # edit
 
