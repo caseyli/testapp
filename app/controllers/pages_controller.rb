e0d99ff8e688aa8d92463a316c0b67dddddd5fb0
Casey Li <casey.li@gmail.com>
2015-03-16 17:57:33 -0400

    Styled page and layout.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
 
   def home 
-    @username = 'caseyli'
-    @user_balance = 240.0
-    @user_due_date = 'January 30th, 2015'
-    @usage_values = [40, 60, 80, 20, 30, 50, 20]
   end
 
 end
